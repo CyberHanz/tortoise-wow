fd9676a3b20592684dbd05d424c4c962baeab754
Hans <hans@local>
2026-09-08 18:07:54 +0200

    Remove local debug and runtime files from repository

--- database.sh
+++
@@ -1,1 +0,0 @@
-for f in sql/database_updates/*.sql; do mysql --force -umangos -pmangos tw_world < "$f"; done
--- database2.sh
+++
@@ -1,4 +0,0 @@
-for f in sql/database_updates/*.sql; do
-  n=$(basename "$f" .sql)
-  mysql -u mangos -pmangos -e "INSERT IGNORE INTO tw_world.migrations (Name, Hash, AppliedAt) VALUES ('$n','manual',NOW());"
-done
